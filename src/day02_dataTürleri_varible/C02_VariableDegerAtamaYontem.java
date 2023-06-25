f1ffa4d2316e74da55baf49470bd96fbd9f02e5c
RamazanKacmaz <98986481+RamazanKacmaz@users.noreply.github.com>
2023-06-26 00:03:35 +0200

    Initial commit

---
+++ src/day02_dataTürleri_varible/C02_VariableDegerAtamaYontem.java
@@ -0,0 +1,13 @@
+package day02_dataTürleri_varible;
+
+public class C02_VariableDegerAtamaYontem {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+
+        int d,e,f ;
+
+        int g=10,k=20,t=30;
+    }
+}
